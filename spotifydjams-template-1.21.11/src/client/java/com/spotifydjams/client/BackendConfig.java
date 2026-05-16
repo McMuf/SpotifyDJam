0aced180615efcd1d3eaa092893a5c7a72234476
Daniel L. <101684691+McMuf@users.noreply.github.com>
2026-05-16 10:10:12 -0700

    Point backend to Railway hosted server

--- spotifydjams-template-1.21.11/src/client/java/com/spotifydjams/client/BackendConfig.java
+++ spotifydjams-template-1.21.11/src/client/java/com/spotifydjams/client/BackendConfig.java
@@ -1,6 +1,5 @@
 package com.spotifydjams.client;
 
 public class BackendConfig {
-    // Your backend URL - we'll change this when deploying later
-    public static final String BASE_URL = "http://127.0.0.1:4000";
+    public static final String BASE_URL = "https://spotifydjam-production.up.railway.app";
 }
